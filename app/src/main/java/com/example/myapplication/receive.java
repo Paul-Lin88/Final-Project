2b2a8204d27d01875e929248e0534e68203a748a
unknown <t109360141@ntut.org.tw>
2023-01-01 21:02:32 +0800

    add,database,receive

--- app/src/main/java/com/example/myapplication/receive.java
+++ app/src/main/java/com/example/myapplication/receive.java
@@ -1,4 +1,71 @@
 package com.example.myapplication;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class receive {
+import android.content.DialogInterface;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class receive extends AppCompatActivity {
+
+    private EditText name=null;
+    private EditText text=null;
+    private Button delete=null;
+    private Button edit=null;
+    private OperateTable mytable =null;
+    private SQLiteOpenHelper helper=null;
+    private String info=null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.receive);
+
+        this.name=(EditText)super.findViewById(R.id.name);
+        this.text=(EditText)super.findViewById(R.id.text);
+        this.delete=(Button)super.findViewById(R.id.delete);
+        this.edit=(Button)super.findViewById(R.id.edit);
+
+        Intent it=super.getIntent();
+        info=it.getStringExtra("info");
+
+        helper=new DataBaseHelp(this);
+        helper.getWritableDatabase();
+        mytable=new OperateTable(helper.getWritableDatabase());
+        tip t=mytable.t(info);
+        name.setText(t.getName());
+        text.setText(t.getText());
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog myAlertDialog = new AlertDialog.Builder(receive.this)
+                        .setTitle("確認" )
+                        .setMessage("確認刪除“"+name.getText()+"”嗎？" )
+                        .setPositiveButton("是" , new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                mytable.delete(info);
+                                Toast.makeText(getApplicationContext(),"刪除成功",Toast.LENGTH_SHORT).show();
+                                receive.this.finish();
+                            }
+                        })
+                        .setNegativeButton("否" , null)
+                        .show();
+            }
+        });
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mytable.updata(info,name.getText().toString(),text.getText().toString());
+                Toast.makeText(getApplicationContext(),"修改成功",Toast.LENGTH_SHORT).show();
+                receive.this.finish();
+            }
+        });
+    }
+
+
 }
